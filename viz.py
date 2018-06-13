a4955e1abfd344023394ce2aa2afdee7cb2c2877
alexandreyc <alexandre.crayssac@gmail.com>
2018-06-13 10:48:34 +0200

    Added logging informations

--- viz.py
+++ viz.py
@@ -1,19 +1,26 @@
+import sys
 import json
-from datetime import datetime
+import logging
+from os import environ
+from datetime import datetime, timedelta
 
 import pandas as pd
 import numpy as np
-from imageio import imread
 from redis import StrictRedis
-from bokeh.plotting import figure, output_notebook, show, curdoc
+from bokeh.plotting import figure, curdoc
 from bokeh.models import ColumnDataSource
 
 
-REDIS_HOST = '127.0.0.1'
-REDIS_PORT = 9999
+REDIS_HOST = environ['REDIS_HOST']
+REDIS_PORT = int(environ['REDIS_PORT'])
 
 IMG_WIDTH = 1920
 IMG_HEIGHT = 1080
+
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO,
+    format='%(module)s|%(name)s|%(filename)s - %(levelname)s @ %(asctime)s : %(message)s'
+)
 
 r = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
 
@@ -23,25 +30,36 @@ def load_ref_data():
     return ref
 
 def load_beacon_pos():
-    return json.load(open('out_pos.json'))
+    return json.load(open('beacons.json'))
 
 ref = load_ref_data()
 pos = load_beacon_pos()
 
-def get_last_states(noise=10):
+def get_last_states(noise=10, max_delta=timedelta(minutes=10)):
     addrs = [t for t in r.scan_iter()]
+    if not addrs:
+        return None
     data = pd.DataFrame(list(map(lambda m: json.loads(m), r.mget(addrs))))
-    #import ipdb; ipdb.set_trace()
-    n_samples, _ = data.shape
     data['datetime'] = data['datetime'].apply(lambda dt: datetime.utcfromtimestamp(dt))
+    data = data[data['datetime'] >= datetime.utcnow() - max_delta]
+    n_samples, _ = data.shape
     data = data.merge(ref, how='left', on='sniffer_addr')
     data['num_beacon'] = data['mdns_name'].str.replace('beacon-|.local', '')
     data['x_beacon'] = data['num_beacon'].apply(lambda s: pos[s][0])
     data['y_beacon'] = data['num_beacon'].apply(lambda s: pos[s][1])
     data['x_beacon'] = data['x_beacon'] + np.random.randint(-noise, noise, size=n_samples)
     data['y_beacon'] = IMG_HEIGHT - data['y_beacon'] + np.random.randint(-noise, noise, size=n_samples)
-    print(data[data['sniffer_addr'] == 'b8:27:eb:36:d8:2f'])
     return data
+
+def compare(prev, cur):
+    prev = prev[['adv_addr', 'sniffer_addr']]
+    cur = cur[['adv_addr', 'sniffer_addr']]
+    data = pd.merge(prev, cur, on='adv_addr', suffixes=('_prev', '_cur'))
+    n_stay = (data['sniffer_addr_prev'] == data['sniffer_addr_cur']).sum()
+    n_moved = (data['sniffer_addr_prev'] != data['sniffer_addr_cur']).sum()
+    if n_moved > 0:
+        logging.info(f'{n_stay} stayed, {n_moved} moved (total = {n_moved + n_stay})')
+        sys.stdout.flush()
 
 fig = figure(
     x_range=(0, IMG_WIDTH), 
@@ -65,16 +83,27 @@ fig.image_url(
 source = ColumnDataSource(data=dict(x=[], y=[]))
 
 fig.circle(
-    x='x', y='y', color='red', line_color='yellow', size=20, alpha=0.5, source=source
+    x='x', y='y', color='red', line_color='yellow', size=10, alpha=0.5, source=source
 )
 
+prev_states = None
+
 def update():
+    global prev_states
     states = get_last_states(noise=20)
+    if states is None:
+        return
+    logging.info(f'min time: {states["datetime"].min()}')
+    logging.info(f'max time: {states["datetime"].max()}')
     data = dict(
         x=states['x_beacon'].values,
         y=states['y_beacon'].values
     )
     source.data = data
+
+    if prev_states is not None:
+        compare(prev_states, states)
+    prev_states = states
 
 curdoc().add_root(fig)
 curdoc().add_periodic_callback(update, 1000)
